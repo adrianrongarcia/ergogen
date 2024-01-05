9678690f03203e3ad9ee906c50bc73c95dc59d56
adrianrongarcia <adrianrongarcia@gmail.com>
2024-01-05 12:09:51 +0100

    Fixes on Mounting Hole footprint

--- src/footprints/openr_footprints/MountingHole.js
+++ src/footprints/openr_footprints/MountingHole.js
@@ -4,12 +4,12 @@ module.exports = {
         designator: 'H',
     },
     body: p => `
-        (module Mounting Hole 2.5mm (layer F.Cu)) 
+        (module MountingHole_2.5mm (layer F.Cu) (tedit 5B24D78E) 
         ${p.at /* parametric position */} 
 
         ${'' /* footprint reference */}        
         (fp_text reference "${p.ref}" (at 0 0) (layer F.SilkS) ${p.ref_hide} (effects (font (size 1.27 1.27) (thickness 0.15))))
-        (fp_text value Jumper (at 0 -7.3) (layer F.Fab) (effects (font (size 1 1) (thickness 0.15))))
+        (fp_text value "" (at 0 0) (layer F.SilkS) hide (effects (font (size 1.27 1.27) (thickness 0.15))))
 
         ${'' /* Hole */}
         (fp_circle (center 0 0) (end 2.75 0) (stroke (width 0.05) (type solid)) (fill none) (layer F.CrtYd))
@@ -17,6 +17,6 @@ module.exports = {
         
         ${'' /* Pads */}
         (pad 1 connect circle (at 0 0) (size 5 5) (layers F.Cu F.Mask))
-        (pad 1 connect circle (at 0 0) (size 5 5) (layers B.Cu B.Mask))
+        (pad 1 connect circle (at 0 0) (size 5 5) (layers B.Cu B.Mask)))
     `
 }
